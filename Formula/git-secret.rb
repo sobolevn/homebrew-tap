7b8853be620fb40f7c5b106282ea0dc07470dc8b
sobolevn <mail@sobolevn.me>
2016-03-18 16:20:41 +0300

    Fixed Formulas/git-secret.rb, now the `brew audit git-secret` passes.
    There were two problems: missing protocol and a new-line at the end of file.

--- Formula/git-secret.rb
+++ Formula/git-secret.rb
@@ -1,6 +1,6 @@
 class GitSecret < Formula
   desc "A bash-tool to store your private data inside a git repository."
-  homepage "sobolevn.github.io/git-secret/"
+  homepage "https://sobolevn.github.io/git-secret/"
 
   url "https://github.com/sobolevn/git-secret/archive/v0.1.0.tar.gz"
   sha256 "107c5555c203ad3b1498e2995fa8aa81942840189316d13933fcf0947180d10b"
