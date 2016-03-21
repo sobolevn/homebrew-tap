3daadb91d820b6d0e05a99f7745fe12357a61d89
sobolevn <mail@sobolevn.me>
2016-03-21 11:37:54 +0300

    Added `caveats` method to the git-secret formula.
    Also an implicit flag `bottle :unneeded` have been added.

--- Formula/git-secret.rb
+++ Formula/git-secret.rb
@@ -1,15 +1,23 @@
 class GitSecret < Formula
-  desc "A bash-tool to store your private data inside a git repository."
+  desc "Bash-tool to store the private data inside a git repo."
   homepage "https://sobolevn.github.io/git-secret/"
 
   url "https://github.com/sobolevn/git-secret/archive/v0.1.0.tar.gz"
   sha256 "107c5555c203ad3b1498e2995fa8aa81942840189316d13933fcf0947180d10b"
 
   head "https://github.com/sobolevn/git-secret.git"
+  bottle :unneeded
 
   def install
     system "make", "build"
     system "bash", "utils/install.sh", prefix
+  end
+
+  def caveats
+    <<-EOS.undent
+      You can now have the `git secret` command! Find out how to use it
+      by typing `man git-secret` or visiting 'https://sobolevn.github.io/git-secret/'.
+    EOS
   end
 
   test do
